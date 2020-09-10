e0543787c12bc591d876398dfa474059b8206d0e
mi5ha6in <mi5ha6in@gmail.com>
2020-09-11 00:01:37 +0300

    fixs error on map

--- source/js/map.js
+++ source/js/map.js
@@ -2,15 +2,14 @@ ymaps.ready(function () {
   var myMap = new ymaps.Map('map', {
       center: [59.956170, 30.309360],
       zoom: 18,
-      controls: ['']
+      controls: []
   });
   myMap.behaviors.disable('scrollZoom');
-  var myPlacemark = new ymaps.Placemark(
+  myPlacemark = new ymaps.Placemark(
       [59.956170, 30.309360] , {
           iconCaption: 'Кронверский проспект, 49'
       }, {
-          iconColor: '#f21f5b',
-
+          iconColor: '#f21f5b'
       });
   myMap.geoObjects.add(myPlacemark);
 });
